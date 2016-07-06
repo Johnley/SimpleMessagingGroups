3f1314afa43e62f4b1e01824bb67ab186ce190dd
Johnley <john@johnconley.me>
2016-07-06 00:21:42 -0400

    Groups and Phones view

---
+++ SimpleMessagingGroups/GroupsAndPhones.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[GroupsAndPhones]
+	AS SELECT        dbo.MessagingGroups.Id AS GroupID, dbo.MessagingGroups.Name AS GroupName, dbo.AddressBook.Email
+FROM            dbo.AddressesInGroups INNER JOIN
+                         dbo.MessagingGroups ON dbo.AddressesInGroups.GroupID = dbo.MessagingGroups.Id LEFT OUTER JOIN
+                         dbo.AddressBook ON dbo.AddressesInGroups.AddressID = dbo.AddressBook.Id
+WHERE        (dbo.MessagingGroups.SendTexts = 1) AND (dbo.AddressBook.RecieveTexts = 1)
